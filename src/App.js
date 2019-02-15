9edcf14e9533fc0107921f5cc4763b07f0b11ac6
Gishtall <Gishtall@gmail.com>
2019-02-15 17:14:30 +0300

    add REAL wokin api))

--- src/App.js
+++ src/App.js
@@ -7,8 +7,10 @@ import Weather from './components/weather';
 const API_KEY ='3db4b74cb073f031caed13e7b405ff48';
 
 class App extends Component {
-  gettingWather = async () => {
-   const api_url = await fetch (`https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=${API_KEY}&units=metric`);
+  gettingWeather = async (event) => {
+    event.preventDefault();
+   const city = event.target.elements.city.value;
+   const api_url = await fetch (`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`);
    const data = await api_url.json();
    console.log(data);
   }
@@ -16,7 +18,7 @@ class App extends Component {
     return (
       <div>
         <Info />
-        <Form />
+        <Form weatherMetod={this.gettingWeather}/>
         <Weather />
       </div>
     );
--- src/components/form.js
+++ src/components/form.js
@@ -2,10 +2,10 @@ import React from 'react';
 class Form extends React.Component {
   render() {
     return (
-      <div>
-          <h1>компонент c формой</h1>
-        
-      </div>
+      <form onSubmit={this.props.weatherMetod}>
+        <input type='text' name='city' placeholder='Город'></input>
+        <button>Узнать погоду</button>
+      </form>
     );
   }
 }
