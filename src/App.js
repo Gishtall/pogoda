0a3727700c47880d8ab2cf4ea333dfef873733b8
Gishtall <Gishtall@gmail.com>
2019-02-15 16:29:15 +0300

    add api

--- src/App.js
+++ src/App.js
@@ -4,7 +4,14 @@ import Info from './components/info';
 import Form from './components/form';
 import Weather from './components/weather';
 
+const API_KEY ='3db4b74cb073f031caed13e7b405ff48';
+
 class App extends Component {
+  gettingWather = async () => {
+   const api_url = await fetch (`https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=${API_KEY}&units=metric`);
+   const data = await api_url.json();
+   console.log(data);
+  }
   render() {
     return (
       <div>
